d16e95d424323ec0e662bd7e66f8e979a466c634
Edy Galdiolo <edygaldy302@gmail.com>
2023-04-13 21:33:26 +0200

    Feat(body): javascript done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
+// Costanti
+
+const stPrice = 0.21;
+
 // Variabili
 
 let distanza;
 let eta;
+let price;
+let discount;
 
 // inputs
 
@@ -10,6 +16,8 @@ distanza = prompt("Inserisci i chilometri che si intende percorrere");
 distanza = parseInt(distanza);
 
 eta = prompt("Inserisci l'età del passeggero");
+
+eta = parseInt(eta);
 
 // checks
 
@@ -22,4 +30,29 @@ else if (isNaN(distanza)) {
 else if (isNaN(eta)) {
     document.getElementById("title").innerText = `Errore, per favore ricarica la pagina e inserisci un numero alla domanda sull'età`;
 }
+else {
+    price = stPrice * distanza;
 
+    if (eta < 17) {
+        discount = price * 20 / 100;
+        price = price - discount;
+    }
+    else if (eta > 64) {
+        discount = price * 40 / 100;
+        price = price - discount;
+    }
+
+    if (!Number.isInteger(price)) {
+        price = price.toFixed(2);
+        price = parseFloat(price);
+    }
+
+    document.getElementById("title").innerText = `Il prezzo del biglietto è: ${price}€`;
+    if (eta < 17) {
+        document.getElementById("disc").innerText = `È stato applicato uno sconto del 20%`;
+    }
+    else if (eta > 64) {
+        document.getElementById("disc").innerText = `È stato applicato uno sconto del 40%`;
+    }
+
+}
